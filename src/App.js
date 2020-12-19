ccafb4b7e7d9ce79898992e6a9856d409aacbd87
pariwesh009 <ajay2322020@outlook.com>
2020-12-19 19:10:59 +0530

    form handling example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Textfield from './components/Textfield';
 import MyButton from './components/mybutton/mybutton';
 import { Userform } from './components/userform/userform';
--- src/components/userform/userform.js
+++ src/components/userform/userform.js
@@ -1,19 +1,50 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 
 
 // function, arrow function
 export class Userform extends Component {//ECMA6 class
-    users = [{ "name": "Ajay" }, { "name": "Ram" }]; //model
-    save() {
-        console.log('clicked');
+    constructor() {
+        super();
+        this.state = {
+            fname: 'Ramesh1',
+            carBrand: 'audi'
+        };
+    }
+    handleChange = (event) => {
+        const value = event.target.type == 'checkbox' ? event.target.checked : event.target.value;
+        this.setState({
+            ...this.state,
+            [event.target.name]: value
+        })
+    }
+    save = () => {
+        console.log('current state of component=', this.state);
     }
     render() {
         return (
-            <div>
-                <input></input>
-                <button onClick={this.save}>Save</button>
-               { this.users.map(u => <div>{u.name}</div>)}
-            </div>
+            <form>
+                <label>First Name:
+                   <input value={this.state.fname} name='fname' onChange={this.handleChange}></input>
+                </label>
+                <label>Last Name:
+                   <input value={this.state.lastname} name='lastname' onChange={this.handleChange}></input>
+                </label>
+                <select name='carBrand' onChange={this.handleChange} value={this.state.carBrand}>
+                    <option value='mercedes'>Mercedes</option>
+                    <option value='bmw'>bmw</option>
+                    <option value='audi'>Audi</option>
+                    <option value='Hyundai'>Hyundai</option>
+                </select>
+                <label><input type='radio' value='male' name='gender' checked={this.state.gender == 'male'}
+                    onChange={this.handleChange} /> Male</label>
+                <label><input type='radio' value='female' name='gender' checked={this.state.gender == 'female'}
+                    onChange={this.handleChange} /> Female</label>
+
+                <label><input type='checkbox' name='domestic' checked={this.state.isChecked}
+                    onChange={this.handleChange} /> domestic</label>
+
+                <button type='button' onClick={this.save}>Save</button>
+            </form>
         )
     }
 }
